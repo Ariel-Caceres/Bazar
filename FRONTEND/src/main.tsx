e06c5f9b4c739c67fce1a2f3d17e3831337e5731
ariel <ariel10player@gmail.com>
2025-06-14 19:44:17 -0300

    responsive-header y resultados

--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
 import { AppProvider } from "../context/contexto.tsx";
+import { ProductosProvider } from '../context/ProductosContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>
-      <App />
+      <ProductosProvider>
+        <App />
+      </ProductosProvider>
     </AppProvider>
   </StrictMode>
 )
